cba29b4080a8aaebee213abce6dac447107e2138
mwild777 <123343357+mwild777@users.noreply.github.com>
2023-10-17 20:10:57 -0600

    Update Time.java

--- src/Time.java
+++ src/Time.java
@@ -1,2 +1,11 @@
+import java.lang.reflect.Field;
 public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+    public Time() {
+        hour = 0;
+        minute = 0;
+        second = 0;
+    }
 }
